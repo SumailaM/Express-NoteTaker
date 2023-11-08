e39243bdafa49c8a0c3b2f21487dc52cf6c0076b
SumailaM <142268062+SumailaM@users.noreply.github.com>
2023-11-07 21:59:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,9 +6,22 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // Html Routes
 app.get('/notes', (req, res) => {
 res.sendFile(__dirname + '/public/notes.html');
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+// api routes
+app.get('/api/notes', (req,res) => {
+    const notes = JSON.parse(fs.readFileSync('db.json'));
+    res.json(notes);
+});
+app.listen(PORT, () => {
+    console.log(`App listening on http://localhost:${PORT}`);
 });
